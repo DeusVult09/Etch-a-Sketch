709b9461dddefda5b2130adce63c02d656448fd0
Zarifa <mamadalievazara@gmail.com>
2025-08-16 00:28:52 +0200

    UI/UX is in progress. Unable to fix the footer at the bottom of the page.

--- main.js
+++ main.js
@@ -1,33 +1,61 @@
+
 const gridContainer = document.getElementById('grid-container');
 const etchASketch = document.querySelector('.etch-a-sketch');
 const clearBtn = document.getElementById('clear');
+const gridSizeControl = document.getElementById('grid-size-control')
+const knobLeft = document.querySelector('.left');
+const knobRight = document.querySelector('.right');
 
 
 const containerSize = parseInt(getComputedStyle(gridContainer).width);
-const gridSize = 12;
-const cellSize = containerSize / gridSize;
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const cell = document.createElement('div');
-    
-    cell.style.width = `${cellSize}px`;
-    cell.style.height = `${cellSize}px`;
-    cell.style.border = '0.3px solid green';
-   
-    gridContainer.appendChild(cell);
+function createGrid(size) {
+    gridContainer.innerHTML = ""; 
+
+    const totalNum = size * size;
+    const cellSize = containerSize / size;
+
+    for (let i = 0; i < totalNum; i++) {
+        const cell = document.createElement('div');
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        cell.style.border = '0.6px dotted green';
+
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'black';
+        });
+
+        cell.addEventListener('click', () => {
+            cell.style.backgroundColor = 'black';
+        });
+
+        gridContainer.appendChild(cell);
+    }
 }
-// shake animation
-clearBtn.addEventListener('click', () => {
-  
-  etchASketch.classList.add('shake');
-  
-  // Clear grid (set all cells back to white)
-  const cells = gridContainer.querySelectorAll('div');
-  cells.forEach(cell => cell.style.background = 'white');
 
-  // Remove animation class after it finishes
-  etchASketch.addEventListener('animationend', () => {
-    etchASketch.classList.remove('shake');
-  }, { once: true });
+
+
+
+createGrid(parseInt(gridSizeControl.value));
+
+
+gridSizeControl.addEventListener("input", (e) => {
+    const size = parseInt(e.target.value);
+    createGrid(size);
+
+    
+    const degrees = (size - 2) * (270 / (50 - 2));
+    e.target.parentElement.style.transform = `rotate(${degrees}deg)`;
+});
+
+
+clearBtn.addEventListener('click', () => {
+    etchASketch.classList.add('shake');
+
+    const cells = gridContainer.querySelectorAll('div');
+    cells.forEach(cell => cell.style.background = 'white');
+    etchASketch.addEventListener('animationend', () => {
+        etchASketch.classList.remove('shake');
+    });
 });
 
