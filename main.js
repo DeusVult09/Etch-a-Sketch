24c25150b6e716f959995c8d9d798c01f24f6be1
Zarifa <mamadalievazara@gmail.com>
2025-08-15 02:00:24 +0200

    Finish: simple UI design of the game.

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-
-
 const gridContainer = document.getElementById('grid-container');
+const etchASketch = document.querySelector('.etch-a-sketch');
+const clearBtn = document.getElementById('clear');
+
+
 const containerSize = parseInt(getComputedStyle(gridContainer).width);
-const gridSize = 16;
+const gridSize = 12;
 const cellSize = containerSize / gridSize;
 
 for (let i = 0; i < gridSize * gridSize; i++) {
     const cell = document.createElement('div');
-    Object.assign(cell.style, {
-        width: `${cellSize}px`,
-        height: `${cellSize}px`,
-        border: '0.5px solid green', // optional: cell border
-    });
+    
+    cell.style.width = `${cellSize}px`;
+    cell.style.height = `${cellSize}px`;
+    cell.style.border = '0.3px solid green';
+   
     gridContainer.appendChild(cell);
 }
+// shake animation
+clearBtn.addEventListener('click', () => {
+  
+  etchASketch.classList.add('shake');
+  
+  // Clear grid (set all cells back to white)
+  const cells = gridContainer.querySelectorAll('div');
+  cells.forEach(cell => cell.style.background = 'white');
+
+  // Remove animation class after it finishes
+  etchASketch.addEventListener('animationend', () => {
+    etchASketch.classList.remove('shake');
+  }, { once: true });
+});
+
